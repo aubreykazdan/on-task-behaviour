c9cdb505891ca67a501d7952e34a9e2d1c2916e6
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-07-26 00:40:31 -0400

    third try

--- otb-next/lib/config.js
+++ otb-next/lib/config.js
@@ -1,6 +1,6 @@
 export const sanityConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "agp8qw4g",
   apiVersion: "v2021-10-21",
   token: process.env.SANITY_API_TOKEN,
   useCdn: true,
