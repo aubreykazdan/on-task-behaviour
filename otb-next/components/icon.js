c430959b389342b98e7c86cbcac98d3e23ed66bb
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-07-29 12:43:16 -0400

    fixed iconbox error

--- otb-next/components/icon.js
+++ otb-next/components/icon.js
@@ -3,11 +3,9 @@ import { SiTiktok } from "react-icons/si";
 
 function IconBox({ children, external, aria }) {
   return (
-    <li>
-      <a aria-label={aria} href={external} rel="noreferrer" target="_blank">
-        {children}
-      </a>
-    </li>
+    <a aria-label={aria} href={external} rel="noreferrer" target="_blank">
+      {children}
+    </a>
   );
 }
 
