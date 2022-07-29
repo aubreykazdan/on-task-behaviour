0fc4006789b5698031c2d47e58a534f45214c257
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-07-29 14:55:07 -0400

    fixed spacing error in grid in TWI

--- otb-next/components/twi.js
+++ otb-next/components/twi.js
@@ -4,14 +4,16 @@ import Img from "./img";
 export default function TextWithIllustration({ copy }) {
   const { content, title, image, alt } = copy;
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 lg:space-x-8">
+    <div className="grid grid-cols-1 lg:grid-cols-2">
       <div className="mb-8 lg:mb-4 lg:w-full flex justify-center lg:justify-start">
         <Img image={image} alt={alt} />
       </div>
 
-      <div className="mb-4 p-0 lg:p-4 lg:w-2/3">
-        <h2 className="text-3xl uppercase text-purple mb-4">{title}</h2>
-        <Content blocks={content} />
+      <div className="mb-4 p-0 lg:p-4 lg:ml-10">
+        <div className="lg:w-3/4 ">
+          <h2 className="text-3xl uppercase text-purple mb-4">{title}</h2>
+          <Content blocks={content} />
+        </div>
       </div>
     </div>
   );
