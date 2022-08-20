e2081c1c7e77b7d0bb5f8aa1ae402d907e6fce63
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-08-20 19:23:27 -0400

    added container to text w/ illustration

--- otb-next/components/sections/twi.js
+++ otb-next/components/sections/twi.js
@@ -1,20 +1,23 @@
 import Content from "../content";
 import Img from "../img";
+import Section from "../section";
 
 export default function TextWithIllustration({ copy }) {
   const { content, title, image, alt } = copy;
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2">
-      <div className="mb-8 lg:mb-4 lg:w-full flex justify-center lg:justify-start">
-        <Img image={image} alt={alt} />
-      </div>
+    <Section container>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="mb-8 lg:mb-4 lg:w-full flex justify-center lg:justify-start">
+          <Img image={image} alt={alt} />
+        </div>
 
-      <div className="mb-4 p-0 lg:p-4 lg:ml-10">
-        <div className="lg:w-3/4 ">
-          <h2 className="text-3xl uppercase text-purple mb-4">{title}</h2>
-          <Content blocks={content} />
+        <div className="mb-4 p-0 lg:p-4 lg:ml-10">
+          <div className="lg:w-3/4 ">
+            <h2 className="text-3xl uppercase text-purple mb-4">{title}</h2>
+            <Content blocks={content} />
+          </div>
         </div>
       </div>
-    </div>
+    </Section>
   );
 }
