f3f20e14ab6a93c83efe972d64603c77372173ea
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-12-07 18:24:57 -0500

    gallery fixed - yarn updated on studio

--- otb-next/pages/gallery/index.js
+++ otb-next/pages/gallery/index.js
@@ -2,7 +2,6 @@ import { getClient } from "../../lib/sanity.server";
 import { useRouter } from "next/router";
 
 import Layout from "../../components/layout";
-// import { galleryQuery } from "../../lib/queries";
 import HeaderBrandedImage from "@/components/layouts/header/headerBrandedImage";
 import Container from "@/components/container";
 
@@ -22,13 +21,10 @@ export default function Gallery({ data }) {
 }
 
 export async function getServerSideProps({ params, preview = false }) {
-  const galleryItems = await getClient(preview).fetch(galleryQuery);
   return {
     props: {
       preview,
-      data: {
-        galleryItems,
-      },
+      data: {},
     },
   };
 }
