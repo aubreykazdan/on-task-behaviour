f87640d5f0ba7058e30e5fdb347ff04362a2e2b1
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2023-02-28 20:27:48 -0500

    shop updated

--- otb-next/pages/shop/index.js
+++ otb-next/pages/shop/index.js
@@ -1,10 +1,10 @@
 import { getClient } from "../../lib/sanity.server";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 import Layout from "../../components/layout";
 import HeaderBrandedImage from "@/components/layouts/header/headerBrandedImage";
-import ContentTwoColumnsImage from "@/components/layouts/content/contentTwoColumnsImage";
-import Link from "next/link";
+import FeatureWithScreenshot from "@/components/layouts/features/featureWithScreenshot";
 
 export default function Shop({ data, products }) {
   const router = useRouter();
@@ -14,7 +14,30 @@ export default function Shop({ data, products }) {
   ) : (
     <Layout data={data}>
       <HeaderBrandedImage title="Shop" />
-      <ContentTwoColumnsImage
+
+      <div className="py-8 sm:py-16">
+        <FeatureWithScreenshot
+          title="Ready for the New Year"
+          imageSrc="/assets/storefront.png"
+          imageAlt="Storefront of On Task"
+        >
+          <p className="text-gray-500">
+            Currently all of our retail merchandise, which typically ranges from
+            in house handmade goods, to unique clothing designs on consignment,
+            is available in person only at 1124 College St in Toronto.
+          </p>
+          <p className="mt-4">
+            Keep watching this space for updates as we prepare to launch our
+            online shop. If you would like to inquire about selling something
+            here or in person, please visit our
+            <span className="nav-animated accent-link">
+              <Link href="/contact"> Contact page.</Link>
+            </span>
+          </p>
+        </FeatureWithScreenshot>
+      </div>
+
+      {/* <ContentTwoColumnsImage
         title="Ready for the New Year"
         imgSrc="/assets/storefront.png"
         imgAlt="cat toes"
@@ -36,7 +59,7 @@ export default function Shop({ data, products }) {
             </Link>
           </p>
         </div>
-      </ContentTwoColumnsImage>
+      </ContentTwoColumnsImage> */}
     </Layout>
   );
 }
