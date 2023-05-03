e9a3276d0d267cc2f990c570dfabd1fd6c3ad3b0
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2023-05-03 18:29:04 -0400

    removed copy schemas temporarily

--- otb-studio/schemas/schema.js
+++ otb-studio/schemas/schema.js
@@ -6,32 +6,16 @@ import siteSettings from "./documents/siteSettings";
 import event from "./documents/event";
 import product from "./documents/product";
 import cta from "./objects/cta";
-import aboutCopy from "./documents/aboutCopy";
-import homeCopy from "./documents/homeCopy";
-import contactCopy from "./documents/contactCopy";
 import form from "./objects/form";
 import gallery from "./documents/gallery";
 import twi from "./objects/twi";
 import eventsList from "./documents/events";
-import shopCopy from "./documents/shopCopy";
-import donateCopy from "./documents/donateCopy";
-import newsCopy from "./documents/newsCopy";
-import galleryCopy from "./documents/galleryCopy";
-import eventsCopy from "./documents/eventsCopy";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: "default",
   types: schemaTypes.concat([
-    // your types here\
-    aboutCopy,
-    homeCopy,
-    contactCopy,
-    eventsCopy,
-    shopCopy,
-    donateCopy,
-    newsCopy,
-    galleryCopy,
+    // your types here
     form,
     blockContent,
     siteSettings,
